3831794b381bb30396013bfc0179ac1bb76f5fce
abock <abock@usgs.gov>
2015-12-08 13:54:27 -0700

    Modified scripts to push out textfiles of VAriance and Conf_UP/Low

--- FF_parser_plain.R
+++ FF_parser_plain.R
@@ -31,7 +31,7 @@ readLine<-function(linenum,infile){
 #**********************************************************
 # enter what directory the EXP files are in
 # and set as working directory
-EXPdir<-"d:/abock/CDOT_Flood_Freq"
+EXPdir<-"W:/hhm/CDOT_Flood_Frequency/Andy"
 setwd(EXPdir)
 #**********************************************************
 
@@ -50,35 +50,51 @@ if (length(FF_files)==0){
 Stats<-list(8,10,15,17)
 ExcProb<-22
 Estimates<-23
+Variance<-24
+Conf_Low<-25
+Conf_Up<-26
 Kval<-27
 
 # build empty lists/matrices to fill with
 # parameters from file
-STAIDs<-c()
+STA_IDs<-c()
 Sdf<-data.frame(matrix(vector(),0,5))
 EPdf<-data.frame(matrix(vector(),0,26))
 Estdf<-data.frame(matrix(vector(),0,26))
+Vdf<-data.frame(matrix(vector(),0,26))
+CLdf<-data.frame(matrix(vector(),0,26))
+CUdf<-data.frame(matrix(vector(),0,26))
 Kdf<-data.frame(matrix(vector(),0,26))
 
 # loop to retreive data and fill lists
 count=1
 for (FF_file in FF_files){
   staid<-unlist(strsplit(FF_file,"[.]"))[1]
-  STAIDs<-append(STAIDs,staid)
+  STA_IDs<-append(STA_IDs,staid)
   Sdf[count,2:5]<-unlist(lapply(Stats,FUN=readLine,infile=FF_file))
   EPdf[count,2:26]<-unlist(lapply(ExcProb,FUN=readLine,infile=FF_file))
   Estdf[count,2:26]<-unlist(lapply(Estimates,FUN=readLine,infile=FF_file))
+  Vdf[count,2:26]<-unlist(lapply(Variance,FUN=readLine,infile=FF_file))
+  CLdf[count,2:26]<-unlist(lapply(Conf_Low,FUN=readLine,infile=FF_file))
+  CUdf[count,2:26]<-unlist(lapply(Conf_Up,FUN=readLine,infile=FF_file))
   Kdf[count,2:26]<-unlist(lapply(Kval,FUN=readLine,infile=FF_file))
   count<-count+1
 }
 
 # format and write out data
-Sdf[,1]<-STAIDs
-colnames(Sdf)<-c("STAID","Skew","StandDev","RegSkew","SysPeaks")
-EPdf[,1]<-STAIDs
-Estdf[,1]<-STAIDs
-Kdf[,1]<-STAIDs
+Sdf[,1]<-STA_IDs
+colnames(Sdf)<-c("STA_ID","Skew","StandDev","RegSkew","SysPeaks")
+EPdf[,1]<-STA_IDs
+Estdf[,1]<-STA_IDs
+Vdf[,1]<-STA_IDs
+CLdf[,1]<-STA_IDs
+CUdf[,1]<-STA_IDs
+Kdf[,1]<-STA_IDs
+
 write.table(Sdf,"Stats.txt",sep=" ",col.names=colnames(Sdf),quote=FALSE,row.names=FALSE)
-write.table(EPdf,"ExProb.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
-write.table(Estdf,"Est.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
+write.table(EPdf,"EXC_Prob.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
+write.table(Estdf,"Estimate.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
+write.table(Vdf,"Variance.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
+write.table(CLdf,"Conf_Low.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
+write.table(CUdf,"Conf_Up.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
 write.table(Kdf,"Kvalue.txt",sep=" ",col.names=FALSE,quote=FALSE,row.names=FALSE)
